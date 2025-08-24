47b985d631f91ee2e22997cb5c133b31d2fc91ef
Sushant Thakur <mail@sushant.uk>
2025-08-24 19:55:58 +0545

    added code for assignment 4

---
+++ assignments/assignment-4/code.py
@@ -0,0 +1,68 @@
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    # Arithmetic operators
+    def __add__(self, other):
+        return Number(self.value + other.value)
+
+    def __sub__(self, other):
+        return Number(self.value - other.value)
+
+    def __mul__(self, other):
+        return Number(self.value * other.value)
+
+    def __truediv__(self, other):
+        return Number(self.value / other.value)
+
+    def __floordiv__(self, other):
+        return Number(self.value // other.value)
+
+    def __mod__(self, other):
+        return Number(self.value % other.value)
+
+    def __pow__(self, other):
+        return Number(self.value ** other.value)
+
+    # Relational operators
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __ne__(self, other):
+        return self.value != other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+
+    def __str__(self):
+        return str(self.value)
+
+
+# Example Usage
+a = Number(10)
+b = Number(5)
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Less Than:", a < b)
+print("Less Than or Equal:", a <= b)
+print("Greater Than:", a > b)
+print("Greater Than or Equal:", a >= b)
+
